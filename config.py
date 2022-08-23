8d7e991b663827091e14881302edabbf459ab83a
senchi <senchpimy@gmail.com>
2022-08-22 19:41:01 -0500

    some changes

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #StartPage='/home/plof/Documents/startpages/Minimo-Homepage/index.html'
-StartPage='/home/plof/Documents/startpages/Starter/homepage.html'
+StartPage='/home/plof/.config/startpage/index.html'
 c.downloads.position = 'bottom'
 c.tabs.background = True
 c.url.default_page = StartPage
@@ -8,10 +8,10 @@ config.load_autoconfig(False)
 
 c.downloads.location.directory = "~/Downloads"
 
-config.bind("mv", "spawn mpv -v {url}")
+config.bind("pv", "spawn mpv -v {url}")
 config.bind("xv", "spawn st -e youtube-dl {url}")
 config.bind("xa", "spawn st -e youtube-dl -x --audio-format mp3 {url}")
-config.set("colors.webpage.darkmode.enabled",True)
+#config.set("colors.webpage.darkmode.enabled",True)
 config.source('qutewal.py')
 
 c.auto_save.interval = 15000
